aa8787638925dc5bb0785c448277eab0da9000ee
AnthonyDWeb <delforge.anthony@gmail.com>
2023-06-07 21:47:42 +0200

    change git url

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import './App.css';
+import ParticulesBackground from './components/background-particules/background.particules';
+import NavigationBar from './components/navigationBar';
+import { DeviseProvider } from './contexts/devise';
+import Homepage from './pages/homepage';
+import AboutMe from './pages/about_me';
+import Contact from './pages/contact';
+import Projets from './pages/projets';
+import Tools from './pages/tools';
+import { StyleProvider } from './contexts/style.context';
+import Error from './pages/error';
 
 function App() {
+  const maintenance = false;
   return (
-    <div className="App">
-     <p> My Portfolio 3</p>
-    </div>
+    <DeviseProvider>
+      <StyleProvider>
+        <ParticulesBackground />
+        {maintenance ?
+          <Error />
+          :
+          <>
+            <NavigationBar />
+            <Homepage />
+            <Tools />
+            <Projets />
+            <AboutMe />
+            <Contact />
+          </>
+        }
+      </StyleProvider>
+    </DeviseProvider>
   );
 }
 
