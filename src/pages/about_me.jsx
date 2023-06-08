3504b6e2dbc52e99bb482c67f2e17d618d87d338
AnthonyDWeb <delforge.anthony@gmail.com>
2023-06-08 09:57:31 +0200

    fix about me

--- src/pages/about_me.jsx
+++ src/pages/about_me.jsx
@@ -11,20 +11,26 @@ export default function AboutMe() {
     <PageContainer style={pageContainerStyle} id='about_me'>
       <GlassmorphismComponent>
         <h2 >À propos de moi</h2>
-        <p >
+        <br />
+        <p>
           Après un cursus initial en biologie inspiré par un besoin continu d'apprentissage et de recherche, je me suis réorienté vers le developpement web et mobile, un métier d'avenir et ayant pour caractéristique un apprentissage continu tout au long de sa carrière ce qui correspond à mes besoins.
         </p>
-
-        <p >
+        <br />
+        <p>
           C'est donc ainsi que j'ai commencé en Mars 2021 une formation de 4 semaines au sein de
-          <a target="_blank" rel="noreferrer" href="https://simplon.co/">Simplon</a>
-          ayant pour but d'initier ses apprenants au codage et à la création d'un prototype d'appplication au cours de celle-ci. <br/>
-          J'ai ensuite poursuivi avec une autre formation de "Devellopeur Web et Mobile Fullstack" entre Mai 2021 et Ocotbre 2021  au sein de
-          <a target="_blank" rel="noreferrer" href="https://www.konexio.eu/index.html">Konexio</a>,
-          celle-ci ayant pour but de professionnaliser ses apprenants à travers une formations complète et très intense. J'ai ensuite effectué un stage de 6mois au sein de
-          <a target="_blank" rel="noreferrer" href="https://www.orchestra.eu/">Orchestra</a>.<br/>
+          <a target="_blank" rel="noreferrer" className='redirectLink' href="https://simplon.co/">Simplon</a>
+          ayant pour but d'initier ses apprenants au codage et à la création d'un prototype d'appplication au cours de celle-ci.
         </p>
-        <p >Plus personellement, j'ai un intérêt prononcé pour les projets liés au secteur du jeu (web ou mobile), de l'apprentissage, de la santé physique (Sport, remise en forme, conseils/motivations) ainsi que les scienses.</p>
+        <br />
+        <p> J'ai ensuite poursuivi avec une autre formation de "Devellopeur Web et Mobile Fullstack" entre Mai 2021 et Octobre 2021  au sein de
+          <a target="_blank" rel="noreferrer" className='redirectLink' href="https://www.konexio.eu/index.html">Konexio</a>,
+          celle-ci ayant pour but de professionnaliser ses apprenants à travers une formations complète et très intense. J'ai ensuite effectué un stage de 6mois au sein de
+          <a target="_blank" rel="noreferrer" className='redirectLink' href="https://www.orchestra.eu/">Orchestra</a>.
+        </p>
+        <br />
+        <p>
+          Plus personellement, j'ai un intérêt prononcé pour les projets liés au secteur du jeu (web ou mobile), de l'apprentissage, de la santé physique (Sport, remise en forme, conseils/motivations) ainsi que les scienses.
+        </p>
       </GlassmorphismComponent>
     </PageContainer>
   )
