dbbaaec79cbfca79cc7d13a8161d78dd969ea1cc
AnthonyDWeb <delforge.anthony@gmail.com>
2023-07-21 18:40:41 +0200

    fix swiper component

--- src/components/swiper/swiper.jsx
+++ src/components/swiper/swiper.jsx
@@ -1,7 +1,7 @@
 // LIBRARY //
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCube } from 'swiper';
+import SwiperCore, { Navigation, Pagination, EffectCube } from "swiper";
 // STYLE //
 import '../../App.css';
 import './swiper.css';
@@ -11,12 +11,13 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 // OTHER
 import { projects } from '../portfolio/project-datalist';
+SwiperCore.use([EffectCube, Navigation, Pagination]);
 
 export default function SwiperComponent(props) {
 const effects = { shadow: true, slideShadows: false, shadowOffset: 20, shadowScale: 0.94 };
     return (
         <div className="container">
-            <Swiper effect={'cube'} grabCursor={true} cubeEffect={effects} loop={true} modules={[EffectCube]}>
+            <Swiper effect='cube' grabCursor={true} cubeEffect={effects} loop={true} pagination={{ clickable: true }} navigation>
                 {projects?.map(d =>
                     <SwiperSlide key={d.source}>
                         <img src={require(`../../assets/projects/${d.source}`)} alt="slide_image" />
