05bd586ba3e17689f8916d4077950ac1e7a49454
AnthonyDWeb <delforge.anthony@gmail.com>
2023-11-05 09:54:25 +0100

    delete form who didn't work on mobile

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,6 @@
 // LIBRARY //
-import React, { useContext, useEffect, useRef, useState } from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
-import emailjs from '@emailjs/browser';
 // STYLE //
 import '../App.css';
 // CONTEXT //
@@ -20,48 +19,6 @@ export default function Contact() {
     const { PageContainer, pageContainerStyle, isLoad } = useContext(StyleContext);
     const { theme, } = useContext(ThemeContext);
 
-    const [isSend, setSend] = useState(false);
-    const form = useRef();
-
-    useEffect(() => { isSend && setTimeout(() => setSend(false), 1000); }, [isSend])
-
-    const sendEmail = (e) => {
-        e.preventDefault();
-        emailjs.sendForm('service_pb82yvi', 'template_lcxba0u', form.current, '690lTCyUWZ-OdtxkO')
-            .then(function (response) {
-                console.log('SUCCESS!', response.status, response.text);
-                setSend(true);
-            }, function (error) {
-                console.log('FAILED...', error);
-            });
-    };
-
-    const Field = ({ placeholder, type, name }) => {
-        return (
-            <FormFields theme={theme} type={type} isSpecial={theme.default === "special"}>
-                {name === "message" ?
-                    <TextAreaContainer name="message" wrap="soft" rows="15" cols="20" placeholder={placeholder} />
-                    :
-                    <input type={type} name={name} placeholder={placeholder} />
-                }
-            </FormFields>
-        )
-    }
-
-    const Form = () => {
-        const btnValue = isSend ? "Email envoyé !" : "Envoyer";
-        return (
-            <GlassmorphismComponent addStyle={{ width: "90vw" }}>
-                <h2 style={{ color: theme.text, marginBottom: 10 }}>Me Contacter</h2>
-                <FormContainer ref={form} onSubmit={sendEmail}>
-                    <Field label={"Nom :"} placeholder={"Qui êtes-vous? ..."} type={"text"} name={"user_name"} />
-                    <Field label={"E-mail :"} placeholder={"Votre e-mail ..."} type={"email"} name={"user_email"} />
-                    <Field label={"Message :"} placeholder={"Votre message ..."} type={""} name={"message"} />
-                    <SubmitButtonContainer type="submit" value={btnValue} theme={theme} />
-                </FormContainer>
-            </GlassmorphismComponent>
-        )
-    };
 
 
     // Media
@@ -81,41 +38,14 @@ export default function Contact() {
 
     return (isLoad &&
         <PageContainer style={{ ...pageContainerStyle }} id='contact' className={"animate__animated animate__backIn"}>
-            <Form />
-            <Media />
+            <GlassmorphismComponent addStyle={{ width: "90vw" }}>
+                <h2 style={{ color: theme.text, marginBottom: 10 }}>Me Contacter</h2>
+                <Media />
+            </GlassmorphismComponent>
         </PageContainer>
     )
 
 }
-
-
-const FormContainer = styled.form`
-display: flex; flex-direction: column; width: 100%; justify-content: center; align-items: center;
-`;
-const FormFields = styled.div`
-display: flex; align-items: center; flex-wrap: wrap; width: 100%; margin-bottom: 1rem; 
-height: auto;
-& textArea, input {
-    min-width: ${props => props.type === "email" ? "60%" : "40%"}; color: ${props => props.theme.text}; height: auto;
-    &::placeholder { color: ${props => props.theme.text}};
-}
-& input {
-    flex-grow: 0; flex-basis: 200px; border-radius: 8px; padding: 5px 10px; outline: none; background: none; height: auto;
-    border-color: ${props => props.isSpecial && props.theme.text}
-}
-& textArea { text-align: justify; border-color: ${props => props.isSpecial && props.theme.text}}
-`;
-
-const TextAreaContainer = styled.textarea`
-background-color: transparent; width: 100%; border-radius: 5px; padding: 5px; text-align: justify; margin-top: 10px; height: auto;
-`;
-
-const SubmitButtonContainer = styled.input`
-cursor: pointer; background: none; padding: 5px 8px; border-radius: 5px; color: ${props => props.theme.text}; border: 1px solid ${props => props.theme.text};
-&:hover {
-    background-color: ${props => props.theme.text}; color: ${props => props.theme.navbar}; font-weight: bold;
-}
-`;
 
 const IconeLink = styled.a`
 display: flex;
